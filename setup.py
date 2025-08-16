6c57e655ef20f6fd2e864eed0e681b42bb1e5c85
bhavjeet singh <bhavjeetsingh784@gmail.com>
2025-08-16 07:22:51 +0530

    pdf uploaded and till vector embeddings the trial is done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='healthwise-chatbot',
+    version='0.1.0',
+    author='Bhavjeet Singh',
+    author_email='bhavjeetsingh784@gmail.com',
+    packages=find_packages(),
+    install_requires=[]     
+)
